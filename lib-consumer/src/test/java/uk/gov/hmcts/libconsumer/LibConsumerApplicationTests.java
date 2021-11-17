de4169c2de7f804788284aa7b0b7e8a1f8e463a0
Alex McAusland <alex.mcausland@gmail.com>
2021-11-17 16:18:48 +0000

    Add consumer project

---
+++ lib-consumer/src/test/java/uk/gov/hmcts/libconsumer/LibConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.libconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
