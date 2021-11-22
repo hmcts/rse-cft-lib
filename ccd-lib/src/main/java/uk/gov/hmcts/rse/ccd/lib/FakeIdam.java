4e0d689ef5372e7b781a3c45ac9c8bb265d550eb
Alex McAusland <alex.mcausland@gmail.com>
2021-11-22 21:00:08 +0000

    ready for user profile

--- ccd-lib/src/main/java/uk/gov/hmcts/rse/ccd/lib/FakeIdam.java
+++ ccd-lib/src/main/java/uk/gov/hmcts/rse/ccd/lib/FakeIdam.java
@@ -19,48 +19,55 @@ import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 public class FakeIdam implements IdamApi {
   @Override
   public UserDetails retrieveUserDetails(String authorisation) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public GeneratePinResponse generatePin(GeneratePinRequest requestBody, String authorisation) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public Response authenticatePinUser(String pin, String clientId, String redirectUri,
                                       String state) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public AuthenticateUserResponse authenticateUser(String authorisation,
                                                    AuthenticateUserRequest authenticateUserRequest) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public TokenExchangeResponse exchangeCode(ExchangeCodeRequest exchangeCodeRequest) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public UserInfo retrieveUserInfo(String authorisation) {
-    return null;
+
+    return UserInfo.builder()
+        .givenName("A")
+        .familyName("Dev")
+        .uid("banderous")
+        .sub("a@b.com")
+        .roles(List.of("sudo"))
+        .build();
   }
 
   @Override
   public TokenResponse generateOpenIdToken(TokenRequest tokenRequest) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public UserDetails getUserByUserId(String authorisation, String userId) {
-    return null;
+    throw new RuntimeException();
   }
 
   @Override
   public List<UserDetails> searchUsers(String authorisation, String elasticSearchQuery) {
-    return null;
+    throw new RuntimeException();
   }
 }
--- lib-consumer/src/test/java/uk/gov/hmcts/libconsumer/LibConsumerApplicationTests.java
+++ lib-consumer/src/test/java/uk/gov/hmcts/libconsumer/LibConsumerApplicationTests.java
@@ -1,13 +1,77 @@
 package uk.gov.hmcts.libconsumer;
 
+import java.util.Collection;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import uk.gov.hmcts.ccd.definition.store.excel.endpoint.ImportController;
+import uk.gov.hmcts.ccd.definition.store.repository.SecurityClassification;
+import uk.gov.hmcts.ccd.definition.store.repository.model.UserRole;
+import uk.gov.hmcts.ccd.definition.store.rest.endpoint.UserRoleController;
 
 @SpringBootTest
 class LibConsumerApplicationTests {
 
+	@Autowired
+	ImportController controller;
+
+	@Autowired
+	UserRoleController roleController;
+
+	@SneakyThrows
+	MockMultipartFile loadNFDivDef() {
+
+		return new MockMultipartFile(
+				"file",
+				"hello.txt",
+				MediaType.MULTIPART_FORM_DATA_VALUE,
+		        getClass().getClassLoader().getResourceAsStream("NFD-dev.xlsx").readAllBytes()
+		);
+	}
+
+	@SneakyThrows
 	@Test
 	void contextLoads() {
+		createRoles(
+		"caseworker-divorce-courtadmin_beta",
+		"caseworker-divorce-superuser",
+		"caseworker-divorce-courtadmin-la",
+		"caseworker-divorce-courtadmin",
+		"caseworker-divorce-solicitor",
+		"caseworker-divorce-pcqextractor",
+		"caseworker-divorce-systemupdate",
+		"caseworker-divorce-bulkscan",
+		"caseworker-caa",
+		"citizen"
+		);
+		Authentication authentication = Mockito.mock(Authentication.class);
+		SecurityContext securityContext = Mockito.mock(SecurityContext.class);
+		Mockito.when(securityContext.getAuthentication()).thenReturn(authentication);
+		SecurityContextHolder.setContext(securityContext);
+
+		Mockito.when(authentication.getPrincipal()).thenReturn(Mockito.mock(Jwt.class));
+
+		controller.processUpload(loadNFDivDef());
+	}
+
+	void createRoles(String... roles) {
+		for (String role : roles) {
+			UserRole r = new UserRole();
+			r.setRole(role);
+			r.setSecurityClassification(SecurityClassification.PUBLIC);
+			roleController.userRolePut(r);
+		}
+
 	}
 
 }
