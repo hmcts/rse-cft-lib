55f3dd2157b3b775975661f65dc2830e9fda5631
Alex McAusland <alex.mcausland@gmail.com>
2021-11-28 20:38:56 +0000

    foo

--- ccd-lib/src/main/java/uk/gov/hmcts/rse/ccd/lib/CCDLibAutoConfigure.java
+++ ccd-lib/src/main/java/uk/gov/hmcts/rse/ccd/lib/CCDLibAutoConfigure.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.gov.hmcts.ccd.AliasWebConfig;
 import uk.gov.hmcts.ccd.CoreCaseDataApplication;
@@ -21,7 +19,6 @@ import uk.gov.hmcts.ccd.auth.AuthCheckerConfiguration;
 import uk.gov.hmcts.ccd.auth.AuthorizedConfiguration;
 import uk.gov.hmcts.ccd.config.SwaggerConfiguration;
 import uk.gov.hmcts.ccd.data.AuthClientsConfiguration;
-import uk.gov.hmcts.ccd.data.SecurityUtils;
 import uk.gov.hmcts.ccd.definition.store.AppInsights;
 import uk.gov.hmcts.ccd.definition.store.CaseDataAPIApplication;
 import uk.gov.hmcts.ccd.definition.store.SecurityConfiguration;
@@ -40,7 +37,7 @@ import uk.gov.hmcts.ccd.hikari.HikariConfigurationPropertiesReportEndpoint;
 })
 @ComponentScan(
     nameGenerator = BeanNamer.class,
-    value = {
+    basePackages = {
     "uk.gov.hmcts.rse.ccd.lib",
     "uk.gov.hmcts.ccd"
 }, excludeFilters = {
