101b7f0ac4e6b2d9ddc10f1cc3a38f29c411caf6
Alex McAusland <alex.mcausland@gmail.com>
2022-01-06 09:37:50 +0000

    fix lib build

--- lib-consumer/src/main/java/uk/gov/hmcts/libconsumer/controller/MyController.java
+++ lib-consumer/src/main/java/uk/gov/hmcts/libconsumer/controller/MyController.java
@@ -3,13 +3,9 @@ package uk.gov.hmcts.libconsumer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.ccd.definition.store.excel.service.ImportService;
 
 @RestController
 public class MyController {
-
-  @Autowired
-  ImportService importer;
 
   @GetMapping(value = "/foo")
   public void foo() {
