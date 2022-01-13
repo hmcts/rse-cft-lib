91801b693f2a389cbab57858b8a6104a365d6f0a
Alex McAusland <alex.mcausland@gmail.com>
2022-01-13 16:30:00 +0000

    add bundled docker compose

--- rse-cft-lib/src/main/java/uk/gov/hmcts/rse/ccd/lib/DBProxy.java
+++ rse-cft-lib/src/main/java/uk/gov/hmcts/rse/ccd/lib/DBProxy.java
@@ -1,24 +1,71 @@
 package uk.gov.hmcts.rse.ccd.lib;
 
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.File;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import javax.sql.DataSource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.SneakyThrows;
 import net.ttddyy.dsproxy.listener.lifecycle.JdbcLifecycleEventListenerAdapter;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.io.FileUtils;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 @Component
 class DBProxy implements BeanPostProcessor {
-  // Set by the RunListener.
-  // TODO
-  public static String applicationPackage;
+  private static volatile String applicationPackage;
+  private static BlockingQueue<LibInfo> queue = new LinkedBlockingQueue<>();
+
+  @AllArgsConstructor
+  @Data
+  static class LibInfo {
+    int dbPort;
+    int esPort;
+  }
+
+  public static class RunListener implements SpringApplicationRunListener {
+    static DockerComposeContainer<?> environment;
+    public RunListener(SpringApplication app, String[] args) {
+      if (applicationPackage == null) {
+        // TODO
+        DBProxy.applicationPackage = app.getMainApplicationClass().getPackageName();
+        new Thread(this::dockerBoot).start();
+      }
+    }
+
+    @SneakyThrows
+    void dockerBoot() {
+      var f = File.createTempFile("cftlib", "");
+      URL u = getClass().getResource("/rse/cftlib-docker-compose.yml");
+      FileUtils.copyURLToFile(u, f);
+      environment =
+          new DockerComposeContainer<>(f)
+              .withExposedService("shared-database", 5432, Wait.forListeningPort())
+              // Allow ES to initialise asynchronously in the background.
+              .withExposedService("ccd-elasticsearch", 9200, Wait.forLogMessage(".*", 1))
+              .withLocalCompose(true);
+      environment.start();
+      var db = environment.getServicePort("shared-database", 5432);
+      var es = environment.getServicePort("ccd-elasticsearch", 9200);
+      queue.put(new LibInfo(db, es));
+    }
+  }
 
   enum project {
     datastore,
@@ -70,8 +117,10 @@ class DBProxy implements BeanPostProcessor {
   private static class ProxyDataSourceInterceptor extends JdbcLifecycleEventListenerAdapter
       implements MethodInterceptor {
     private final DataSource dataSource;
+    private final HikariDataSource hikari;
 
     public ProxyDataSourceInterceptor(final DataSource dataSource) {
+      this.hikari = (HikariDataSource) dataSource;
       this.dataSource = ProxyDataSourceBuilder
           .create(dataSource)
           .listener(this)
@@ -84,6 +133,7 @@ class DBProxy implements BeanPostProcessor {
       Method proxyMethod =
           ReflectionUtils.findMethod(dataSource.getClass(), invocation.getMethod().getName());
       if (proxyMethod != null) {
+        checkDBIsReady();
         Object result = proxyMethod.invoke(dataSource, invocation.getArguments());
         if (invocation.getMethod().getName().equals("getConnection")) {
           StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
@@ -103,5 +153,15 @@ class DBProxy implements BeanPostProcessor {
       }
       return invocation.proceed();
     }
+
+    @SneakyThrows
+    private void checkDBIsReady() {
+      if (!initialised) {
+        var info = queue.take();
+        hikari.setJdbcUrl(String.format("jdbc:postgresql://localhost:%s/postgres?stringtype=unspecified", info.getDbPort()));
+        initialised = true;
+      }
+    }
+    boolean initialised;
   }
 }
