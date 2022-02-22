9aa626e865f535c0e79fb12a5d2627847336c935
Alex McAusland <alex.mcausland@gmail.com>
2022-02-22 11:27:52 +0000

    add gradle plugin project

---
+++ rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/CftLibPlugin.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.rse;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class CftLibPlugin implements Plugin<Project> {
+
+  public void apply(Project project) {
+
+  }
+}
