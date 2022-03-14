c3fb939683fb23d565af0a6bab7b201421ba0080
banderous <alex.mcausland@gmail.com>
2022-03-14 10:06:15 +0000

    Tidy gradle plugin (#117)

--- rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/CftLibPlugin.java
+++ rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/CftLibPlugin.java
@@ -5,11 +5,13 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
@@ -38,26 +40,40 @@ public class CftLibPlugin implements Plugin<Project> {
     public void apply(Project project) {
         project.getPlugins().apply("java");
 
+
+        createSourceSets(project);
+        createConfigurations(project);
+
+        createManifestTasks(project);
+        createBootWithCCDTask(project);
+        createTestTask(project);
+    }
+
+    private void createConfigurations(Project project) {
+        project.getConfigurations().getByName("cftlibImplementation")
+            .extendsFrom(project.getConfigurations().getByName("implementation"))
+            .getDependencies().addAll(List.of(libDependencies(project, "app-runtime", "rse-cft-lib")));
+
+        project.getConfigurations().getByName("cftlibRuntimeOnly")
+            .extendsFrom(project.getConfigurations().getByName("runtimeOnly"));
+        project.getConfigurations().getByName("cftlibTestImplementation")
+            .extendsFrom(project.getConfigurations().getByName("cftlibImplementation"))
+            .getDependencies().addAll(List.of(
+                project.getDependencies().create("org.junit.platform:junit-platform-console-standalone:1.8.2"),
+                project.getDependencies().create("com.github.hmcts.rse-cft-lib:test-runner:" + getLibVersion(project))
+            ));
+
+        project.getConfigurations().getByName("cftlibTestRuntimeOnly")
+            .extendsFrom(project.getConfigurations().getByName("cftlibRuntimeOnly"));
+    }
+
+    private void createSourceSets(Project project) {
         SourceSetContainer s = project.getExtensions().getByType(SourceSetContainer.class);
         s.add(s.create("cftlib", x -> {
             var main = s.getByName("main").getOutput();
             x.setCompileClasspath(x.getCompileClasspath().plus(main));
             x.setRuntimeClasspath(x.getRuntimeClasspath().plus(main));
         }));
-
-        project.getConfigurations().getByName("cftlibImplementation")
-            .extendsFrom(project.getConfigurations().getByName("implementation"));
-
-        project.afterEvaluate(x -> {
-            project.getConfigurations().getByName("cftlibImplementation")
-                .getDependencies().addAll(List.of(
-                    project.getDependencies().create("com.github.hmcts.rse-cft-lib:app-runtime:" + getLibVersion(project)),
-                    project.getDependencies().create("com.github.hmcts.rse-cft-lib:rse-cft-lib:" + getLibVersion(project))
-                ));
-        });
-
-        project.getConfigurations().getByName("cftlibRuntimeOnly")
-            .extendsFrom(project.getConfigurations().getByName("runtimeOnly"));
 
         s.add(s.create("cftlibTest", x -> {
             var cftlib = s.getByName("cftlib").getOutput();
@@ -66,24 +82,6 @@ public class CftLibPlugin implements Plugin<Project> {
             x.setCompileClasspath(x.getCompileClasspath().plus(cftlib).plus(main));
             x.setRuntimeClasspath(x.getRuntimeClasspath().plus(cftlib).plus(main));
         }));
-
-        createTestSourceSet(project);
-
-        createManifestTasks(project);
-        createBootWithCCDTask(project);
-        createTestTask(project);
-    }
-
-    private void createTestSourceSet(Project project) {
-        var impl = project.getConfigurations().getByName("cftlibTestImplementation");
-        impl.extendsFrom(project.getConfigurations().getByName("cftlibImplementation"));
-
-        project.getConfigurations().getByName("cftlibTestRuntimeOnly")
-            .extendsFrom(project.getConfigurations().getByName("cftlibRuntimeOnly"));
-
-        impl.getDependencies().add(project.getDependencies().create("org.junit.platform:junit-platform-console-standalone:1.8.2"));
-        // Wait until build script evaluation to get lib version.
-        project.afterEvaluate(x -> impl.getDependencies().add(project.getDependencies().create("com.github.hmcts.rse-cft-lib:test-runner:" + getLibVersion(project))));
     }
 
     private void createBootWithCCDTask(Project project) {
@@ -140,11 +138,9 @@ public class CftLibPlugin implements Plugin<Project> {
     }
 
     private Task createCFTManifestTask(Project project, String depName, String mainClass, File file, String... args) {
-        return project.task("writeManifest" + depName)
-            .doFirst(x -> {
-                Configuration classpath = cftConfiguration(project, depName);
-                writeManifest(project, classpath, mainClass, file, args);
-            });
+        Configuration classpath = project.getConfigurations().detachedConfiguration(
+            libDependencies(project, depName, "injected"));
+        return createManifestTask(project, "writeManifest" + depName, classpath, mainClass, file, args);
     }
 
     private Task createManifestTask(Project project, String name, FileCollection configuration, String mainClass, File file, String... args) {
@@ -152,21 +148,6 @@ public class CftLibPlugin implements Plugin<Project> {
             .doFirst(x -> {
                 writeManifest(project, configuration, mainClass, file, args);
             });
-    }
-
-    private Configuration cftConfiguration(Project project, String name) {
-        return configuration(project,
-            "com.github.hmcts.rse-cft-lib:" + name + ":" + getLibVersion(project),
-            "com.github.hmcts.rse-cft-lib:injected:" + getLibVersion(project)
-        );
-    }
-
-    private Configuration configuration(Project project, String... dependencies) {
-        var deps = Arrays.stream(dependencies).map(
-            x -> project.getDependencies().create(x)
-        ).toArray(Dependency[]::new);
-
-        return project.getConfigurations().detachedConfiguration(deps);
     }
 
     @SneakyThrows
@@ -232,20 +213,17 @@ public class CftLibPlugin implements Plugin<Project> {
         // Allow more time for definitions to import to reduce test flakeyness
         j.environment("CCD_TX-TIMEOUT_DEFAULT", "120");
 
-
         j.environment("SPRING_SECURITY_OAUTH2_CLIENT_PROVIDER_OIDC_ISSUER_URI",
             "https://idam-web-public.aat.platform.hmcts.net/o");
 
 //        j.environment("LOGGING_LEVEL_ORG_SPRINGFRAMEWORK_SECURITY", "DEBUG");
 
-        setJvmArgs(j);
+        setRequiredJvmArgs(j);
 
-        // This needs to happen after evaluation so the lib version is set in the build.gradle.
         j.doFirst(x -> {
             // Resolve the configuration as a detached configuration for isolation from
             // the project's build (eg. to prevent interference from spring boot's dependency mgmt plugin)
-            Configuration classpath = configuration(project, "com.github.hmcts.rse-cft-lib:rse-cft-lib:" + getLibVersion(project));
-            j.classpath(classpath);
+            j.classpath(project.getConfigurations().detachedConfiguration(libDependencies(project, "rse-cft-lib")));
         });
 
         j.args(manifests);
@@ -253,7 +231,7 @@ public class CftLibPlugin implements Plugin<Project> {
         return j;
     }
 
-    void setJvmArgs(JavaExec j) {
+    void setRequiredJvmArgs(JavaExec j) {
         j.jvmArgs("-Xverify:none");
         j.jvmArgs("-XX:TieredStopAtLevel=1");
 
@@ -264,6 +242,12 @@ public class CftLibPlugin implements Plugin<Project> {
         });
         j.jvmArgs("--add-opens", "java.desktop/java.awt.font=ALL-UNNAMED");
         j.jvmArgs("-XX:ReservedCodeCacheSize=64m");
+    }
+
+    Dependency[] libDependencies(Project project, String... libDeps) {
+        return Arrays.stream(libDeps)
+            .map(d -> project.getDependencies().create("com.github.hmcts.rse-cft-lib:" + d + ":" + getLibVersion(project)))
+            .toArray(Dependency[]::new);
     }
 
     Directory getBuildDir(Project project) {
