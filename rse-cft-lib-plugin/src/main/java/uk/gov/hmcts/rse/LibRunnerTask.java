049e0c7046ebf4e8ee387ebc22cc8c15381e6fba
Alex McAusland <alex.mcausland@gmail.com>
2022-07-06 20:33:55 +0100

    test

--- rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/LibRunnerTask.java
+++ rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/LibRunnerTask.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.rse;
 
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.SneakyThrows;
+import org.apache.tools.ant.taskdefs.condition.Os;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 import org.gradle.jvm.toolchain.JavaToolchainService;
@@ -66,9 +68,12 @@ public class LibRunnerTask extends JavaExec {
         var env = getProject().file("build/cftlib/.aat-env");
         if (!env.exists()) {
             try (var os = new FileOutputStream(getProject().file(env))) {
+                var cmd = Os.isFamily(Os.FAMILY_WINDOWS)
+                    ? new ArrayList<>(List.of("cmd", "/c"))
+                    : new ArrayList<String>();
+                cmd.addAll(List.of("az", "keyvault", "secret", "show", "-o", "tsv", "--query", "value", "--id", "https://rse-cft-lib.vault.azure.net/secrets/aat-env"));
                 getProject().exec(x -> {
-                    x.commandLine("az", "keyvault", "secret", "show", "-o", "tsv", "--query", "value", "--id",
-                        "https://rse-cft-lib.vault.azure.net/secrets/aat-env");
+                    x.commandLine(cmd);
                     x.setStandardOutput(os);
                 });
             }
