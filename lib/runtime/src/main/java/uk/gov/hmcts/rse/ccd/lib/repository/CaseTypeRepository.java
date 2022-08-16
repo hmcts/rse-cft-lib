231e03c16a3c58c83433e3c2f2d61afc20ba8445
Linus Norton <linusnorton@gmail.com>
2022-08-16 11:37:14 +0100

    Clean up

--- lib/runtime/src/main/java/uk/gov/hmcts/rse/ccd/lib/repository/CaseTypeRepository.java
+++ lib/runtime/src/main/java/uk/gov/hmcts/rse/ccd/lib/repository/CaseTypeRepository.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.rse.ccd.lib.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.definition.store.repository.model.CaseType;
-import uk.gov.hmcts.rse.ccd.lib.config.DefinitionConfig;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,14 +24,5 @@ public class CaseTypeRepository {
         var caseType = new CaseType();
 
         return caseType;
-    }
-
-    @ConfigurationProperties(prefix = "ccd.definition")
-    public static class Properties {
-        public Map<String, String> getPaths() {
-            return paths;
-        }
-
-        private final Map<String, String> paths = new HashMap<>();
     }
 }
