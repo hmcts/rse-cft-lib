2433c1c152d70c6d5d3eff8a28d77eebf138748e
Alex McAusland <a.m@user.com>
2023-06-29 13:58:11 +0100

    test idam client presence

--- cftlib/test-project/src/main/java/uk/gov/hmcts/libconsumer/LibConsumerApplication.java
+++ cftlib/test-project/src/main/java/uk/gov/hmcts/libconsumer/LibConsumerApplication.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.libconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.idam"})
 @SpringBootApplication
 public class LibConsumerApplication {
 
