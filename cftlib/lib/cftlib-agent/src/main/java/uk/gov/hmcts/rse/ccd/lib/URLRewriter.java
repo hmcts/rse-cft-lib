da2c8d4072b490dd2d576cf817be1bf2c392b691
Alex McAusland <a.m@user.com>
2024-01-15 09:50:54 +0000

    tmp

--- cftlib/lib/cftlib-agent/src/main/java/uk/gov/hmcts/rse/ccd/lib/URLRewriter.java
+++ cftlib/lib/cftlib-agent/src/main/java/uk/gov/hmcts/rse/ccd/lib/URLRewriter.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -28,6 +29,9 @@ public class URLRewriter extends OncePerRequestFilter {
     private final IdamApi idam;
 
     private final String name;
+
+    @Value("#{T(org.springframework.boot.SpringBootVersion).getVersion()}")
+    private String springBootVersion;
 
     @Autowired
     public URLRewriter(IdamApi idam,
