a2b5ae3daad9389175cdb4668659679c51b140cf
Alex McAusland <a.m@user.com>
2025-05-14 16:00:50 +0100

    fix task dependencies

--- cftlib/rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/ManifestTask.java
+++ cftlib/rse-cft-lib-plugin/src/main/java/uk/gov/hmcts/rse/ManifestTask.java
@@ -1,10 +1,14 @@
 package uk.gov.hmcts.rse;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.InputFiles;
 
 @RequiredArgsConstructor
+@Getter
 public class ManifestTask extends DefaultTask {
+    @InputFiles
     public FileCollection classpath;
 }
